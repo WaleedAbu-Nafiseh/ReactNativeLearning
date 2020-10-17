113f32d732c621e965b6808b7112a5a4025baf27
WaleedAbu-Nafiseh <>
2020-10-17 20:00:10 +0300

    Updated packages, used useState , added TouchableHighlight and in a function

--- App.js
+++ App.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Dimensions,
   Image,
   View,
+  TouchableHighlight,
   ProgressViewIOS,
   ProgressBarAndroid,
   ActivityIndicator,
-    Button,
+  Button,
+  Alert,
   StyleSheet,
   Text,
+  Platform,
   StatusBar,
 } from 'react-native';
 import picLogo from './assets/favicon.png';
+const {height, width} = Dimensions.get('window');
 const App: () => React$Node = () => {
+  const [backgroundColor, setBackgroundColor] = useState('blue');
   const onPressed = () => {
-    alert('Hello');
+    Alert.alert('Theh OS is ' + Platform.OS);
   };
+  function ColorButon({backgroundColor, onPress = (f) => f}) {
+    return (
+      <TouchableHighlight
+        style={styles.button}
+        underlayColor={backgroundColor}
+        onPress={() => onPress(backgroundColor)}>
+        <View style={styles.row}>
+          <View style={[styles.sample, {backgroundColor}]} />
+          <Text style={styles.defaultText}>{backgroundColor}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
   return (
     <>
       <StatusBar hidden={true} />
-      <View style={styles.container}>
-        <ActivityIndicator size="large" color="#61DBFB" />
-        <Button title="ClickMe!" onPress={onPressed} />
-        <Text style={styles.defaultText}>وليد</Text>
-        <Text style={[styles.selectedText, styles.defaultText]}>محمد</Text>
-        <Text style={styles.defaultText}>باسل</Text>
+      <View style={[styles.container, {backgroundColor}]}>
+        <ColorButon backgroundColor={'red'} onPress={backgroundColor} />
+        <ColorButon backgroundColor={'blue'} onPress={backgroundColor} />
+        <ColorButon backgroundColor={'green'} onPress={backgroundColor} />
+        <Button title="ClickMe you stupid person!\n" onPress={onPressed} />
       </View>
     </>
   );
@@ -35,8 +52,24 @@ const styles = StyleSheet.create({
     fontSize: 22,
     padding: 10,
     margin: 5,
-    borderWidth: StyleSheet.hairlineWidth,
-    color: 'red',
+  },
+  sample: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    backgroundColor: 'green',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  button: {
+    margin: 10,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    alignSelf: 'stretch',
+    backgroundColor: 'rgba(255,255,255,0.7)',
   },
   picStyle: {
     borderRadius: 100,
